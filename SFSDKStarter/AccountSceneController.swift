9f0cba38fd5fb86a1c25fde24e5d4c51e071f3c0
victor <ruizdvictor@gmail.com>
2019-12-31 19:19:11 -0500

    added functionality to retrieve accounts and store in dictionary

--- SFSDKStarter/AccountSceneController.swift
+++ SFSDKStarter/AccountSceneController.swift
@@ -14,4 +14,25 @@ class AccountSceneController: UITableViewController {
     
    var dataRows = [Dictionary<String, Any>]()
     
+    // MARK: - View lifecycle
+    override func loadView() {
+       super.loadView()
+       self.title = "Accounts"
+       let request = RestClient.shared.request(forQuery: "SELECT Id, Name FROM Account LIMIT 10")
+       RestClient.shared.send(request: request, onFailure: { (error, urlResponse) in
+           SalesforceLogger.d(type(of:self), message:"Error invoking: \(request)")
+       }) { [weak self] (response, urlResponse) in
+           guard let strongSelf = self,
+                 let jsonResponse = response as? Dictionary<String,Any>,
+                 let result = jsonResponse ["records"] as? [Dictionary<String,Any>]  else {
+                   return
+                 }
+           SalesforceLogger.d(type(of:strongSelf),message:"Invoked: \(request)")
+           DispatchQueue.main.async {
+               strongSelf.dataRows = result
+               strongSelf.tableView.reloadData()
+           }
+       }
+    }
+    
 }
