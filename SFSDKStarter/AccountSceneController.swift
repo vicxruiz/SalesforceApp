9c10b98349982a5a1a5d75c02ac7e7ec654613ad
victor <ruizdvictor@gmail.com>
2019-12-31 19:10:59 -0500

    created array of dictionaries for accounts

--- SFSDKStarter/AccountSceneController.swift
+++ SFSDKStarter/AccountSceneController.swift
@@ -1,6 +1,17 @@
+//
+//  AccountSceneController.swift
+//  SFSDKStarter
+//
+//  Created by Victor  on 12/31/19.
+//  Copyright © 2019 Salesforce. All rights reserved.
+//
+
+import Foundation
 import UIKit
 import SalesforceSDKCore
+
 class AccountSceneController: UITableViewController {
-  var dataRows = [Dictionary<String, Any>]()
-  
+    
+   var dataRows = [Dictionary<String, Any>]()
+    
 }
